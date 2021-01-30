270ad90ae33a9054cd57b1e71145560b09652def
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-01-30 19:08:55 +0200

    Add Agent class

--- leads/models.py
+++ leads/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
 
 
 class Lead(models.Model):
@@ -12,6 +15,7 @@ class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
 
     phoned = models.BooleanField(default=False)
     source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
@@ -19,4 +23,9 @@ class Lead(models.Model):
     profile_picture = models.ImageField(blank=True, null=True)
     special_fields = models.FileField(blank=True, null=True)
 
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
 
