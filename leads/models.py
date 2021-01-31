ed9ccb7e504c502ec47575f7fbee75b04463ca33
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-01-31 09:49:15 +0200

    Add Querysets and Managers

--- leads/models.py
+++ leads/models.py
@@ -25,6 +25,9 @@ class Lead(models.Model):
     profile_picture = models.ImageField(blank=True, null=True)
     special_fields = models.FileField(blank=True, null=True)
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
