9f6848d267d5544a4376a5ab6dd7039d37267423
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-01-31 17:46:20 +0200

    Add submission of the data nd redirection @ forms

--- leads/views.py
+++ leads/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .forms import LeadForm
-from .models import Lead
+from .models import Agent, Lead
 # Create your views here.
 
 
@@ -23,7 +23,22 @@ def lead_detail(request, pk):
 
 
 def lead_create(request):
+    form = LeadForm()
+    if request.method == "POST":
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+            Lead.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                age=age,
+                agent=agent,
+            )
+            return redirect("/leads")
     context = {
-        "form": LeadForm()
+        "form": form
     }
     return render(request, 'lead_create.html', context)
