4d11bcfecc2fde6affb922cdf11ba7b123ced1ea
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-02 06:44:31 +0200

    Add landing_page

--- leads/views.py
+++ leads/views.py
@@ -6,6 +6,10 @@ from .models import Agent, Lead
 # Create your views here.
 
 
+def landing_page(request):
+    return render(request, "landing.html")
+
+
 def lead_list(request):
     leads = Lead.objects.all()
     context = {
