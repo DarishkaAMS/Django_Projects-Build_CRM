a5ef04d21fbc5d2fb4fbfa6e631693e96e0c9f03
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-06 10:36:27 +0200

    Add test_get_request @ tests.py (optimizing)

--- leads/tests.py
+++ leads/tests.py
@@ -6,14 +6,9 @@ from django.shortcuts import reverse
 
 class LandingPageTest(TestCase):
 
-    def test_status_code(self):
+    def test_get_request(self):
         response = self.client.get(reverse("landing-page"))
-        print(response.content)
-        print(response.status_code)
         self.assertEqual(response.status_code, 200)
-
-    def test_template_name(self):
-        response = self.client.get(reverse("landing-page"))
         self.assertTemplateUsed(response, "landing.html")
 
 
