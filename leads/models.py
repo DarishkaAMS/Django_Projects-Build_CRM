c5c851562289fe9cbb617ba7ff9153f6e14e4987
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-06 17:16:05 +0200

    Add post_user_created_signal @ models.py

--- leads/models.py
+++ leads/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
@@ -42,3 +43,12 @@ class Agent(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+def post_user_created_signal(sender, instance, created, **kwargs):
+    print(sender)
+    print(instance)
+    print(created)
+
+
+post_save.connect(post_user_created_signal, sender=User)
