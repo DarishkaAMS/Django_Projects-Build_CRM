6fc05ec53b13af6b0c4b87a724ab234b19781fb8
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-06 10:23:09 +0200

    Add LandingPageTest @ tests.py

--- leads/tests.py
+++ leads/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
 # Create your tests here.
+
+
+class LandingPageTest(TestCase):
+
+    def test_status_code(self):
+        self.client.get(reverse("landing-page"))
+        pass
+
+    def test_template_neme(self):
+        pass
+
