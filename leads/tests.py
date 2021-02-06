4d08cc3baee78abcac56f6c673b654f0dad9184a
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-06 10:34:58 +0200

    Add test_template_name @ tests.py

--- leads/tests.py
+++ leads/tests.py
@@ -11,8 +11,9 @@ class LandingPageTest(TestCase):
         print(response.content)
         print(response.status_code)
         self.assertEqual(response.status_code, 200)
+
+    def test_template_name(self):
+        response = self.client.get(reverse("landing-page"))
         self.assertTemplateUsed(response, "landing.html")
 
-    # def test_template_neme(self):
-    #     pass
 
