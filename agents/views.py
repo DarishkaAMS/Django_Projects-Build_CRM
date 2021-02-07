3a360719ea4bbdba291a4b90f0a0cafa38b80386
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-07 09:06:26 +0200

    Add AgentListView and AgentCreateView

--- agents/views.py
+++ agents/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import reverse
 from django.views import generic
 
+from .forms import AgentModelForm
 from leads.models import Agent
 
 # Create your views here.
@@ -16,7 +17,15 @@ class AgentListView(LoginRequiredMixin, generic.ListView):
 
 class AgentCreateView(LoginRequiredMixin, generic.CreateView):
     template_name = "agents/agent_create.html"
-    form_class = None
+    form_class = AgentModelForm
 
     def get_success_url(self):
-        return reverse('agent:agent-list')
+        return reverse('agents:agent-list')
+
+    def form_valid(self, form):
+        agent = form.save(commit=False)
+        agent.organization = self.request.user.userprofile
+        agent.save()
+    # self.object = form.save()
+    # return super().form_valid(form)
+        return super(AgentCreateView, self).form_valid(form)
