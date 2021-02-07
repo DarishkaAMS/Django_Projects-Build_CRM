05f1ece839902fc37fc17b532002859dc63bbfaf
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-07 16:15:41 +0200

    Add password_reset_complete.html

--- DarishkaAMS_CRM/urls.py
+++ DarishkaAMS_CRM/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import (LoginView, LogoutView,
+                                       PasswordResetCompleteView,
                                        PasswordResetConfirmView,
                                        PasswordResetDoneView, PasswordResetView)
 from django.urls import path, include
@@ -17,8 +18,9 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name="logout"),
     path('signup/', SignupView.as_view(), name="signup"),
     path('reset-password/', PasswordResetView.as_view(), name="reset-password"),
-    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name="password-reset-confirm"),
-    path('password-reset-done/', PasswordResetDoneView.as_view(), name="password-reset-done"),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
 
 if settings.DEBUG:
