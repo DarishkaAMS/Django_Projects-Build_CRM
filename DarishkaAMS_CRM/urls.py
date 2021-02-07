796ec42b10c5cc5ef9f407f4de376c2976b04508
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-07 15:37:49 +0200

    Add password_reset_confirm/done/form.html

--- DarishkaAMS_CRM/urls.py
+++ DarishkaAMS_CRM/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
 from django.urls import path, include
 from leads.views import landing_page, LandingPageView, SignupView
 
@@ -29,6 +29,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
     path('signup/', SignupView.as_view(), name="signup"),
+    path('reset-password/', PasswordResetView.as_view(), name="reset-password"),
 ]
 
 if settings.DEBUG:
