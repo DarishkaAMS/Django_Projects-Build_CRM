18a7e00b8add736023e9c6cf9c2e5d5732409978
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-07 15:49:59 +0200

    Add password_reset_done/email.html

--- DarishkaAMS_CRM/urls.py
+++ DarishkaAMS_CRM/urls.py
@@ -1,22 +1,8 @@
-"""DarishkaAMS_CRM URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
+from django.contrib.auth.views import (LoginView, LogoutView,
+                                       PasswordResetDoneView, PasswordResetView)
 from django.urls import path, include
 from leads.views import landing_page, LandingPageView, SignupView
 
@@ -30,6 +16,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name="logout"),
     path('signup/', SignupView.as_view(), name="signup"),
     path('reset-password/', PasswordResetView.as_view(), name="reset-password"),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name="password-reset-done"),
 ]
 
 if settings.DEBUG:
