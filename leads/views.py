2620c7e216f905512982470f7fdcda4cf5affcec
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-08 07:06:19 +0200

    Add category_detail.html

--- leads/views.py
+++ leads/views.py
@@ -224,6 +224,24 @@ class CategoryListView(LoginRequiredMixin, generic.ListView):
 
 class CategoryDetailView(LoginRequiredMixin, generic.DetailView):
     template_name = "category_detail.html"
+    context_object_name = "category"
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryDetailView, self).get_context_data(**kwargs)
+        user = self.request.user
+
+        if user.is_organizer:
+            queryset = Lead.objects.filter(
+                organization=user.userprofile
+            )
+        else:
+            queryset = Lead.objects.filter(
+                organization=user.agent.organization
+            )
+        context.update({
+            "unassigned_lead_count": queryset.filter(category__isnull=True).count()
+        })
+        return context
 
     def get_queryset(self):
         user = self.request.user
