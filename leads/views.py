f1c067105826f1e4a8ce66291ac4900115388da9
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-08 06:17:58 +0200

    Add unassigned_lead_count

--- leads/views.py
+++ leads/views.py
@@ -197,6 +197,20 @@ class CategoryListView(LoginRequiredMixin, generic.ListView):
     template_name = "category_list.html"
     context_object_name = "category_list"
 
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        user = self.request.user
+
+        if user.is_organizer:
+            queryset = Lead.objects.filter(organization=user.userprofile)
+        else:
+            queryset = Lead.objects.filter(organization=user.agent.organization)
+
+        context.update({
+            "unassigned_lead_count": queryset.filter(category__isnull=True).count()
+        })
+        return context
+
     def get_queryset(self):
         user = self.request.user
 
