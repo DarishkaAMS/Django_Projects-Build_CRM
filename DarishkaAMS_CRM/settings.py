4ff5c7e2d3ecc7a35b8a4823c1fd69705b436a30
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-09 06:45:19 +0200

    Install crispy_forms

--- DarishkaAMS_CRM/settings.py
+++ DarishkaAMS_CRM/settings.py
@@ -38,8 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local apps
     'agents',
     'leads',
+
+    # third party apps
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
